0f4f9ec097c908539756ac89625f5adccea2c430
Octavian Irimia <octavian.irimia0309@gmail.com>
2024-01-22 20:27:35 +0200

    Improved download function

--- Code/download_video.py
+++ Code/download_video.py
@@ -4,15 +4,6 @@ import ssl
 
 
 
-# Check if url is accesible
-def check_url(url: str) -> None:
-    
-    request: requests.Response = requests.get(url = url)
-    print(request.status_code)
-    return request.status_code == 200
-
-
-
 # Get all resolutions of the video
 def get_video_resolutions(url: str):
 
@@ -40,10 +31,7 @@ def download_video(url: str, resolution: str) -> None:
     ssl._create_default_https_context = ssl._create_stdlib_context
 
     try:
-        print(resolution)
         pytube.YouTube = pytube.YouTube(url = url).streams.filter(resolution = resolution).first().download()
-
-        print('Video was downloaded!')
     
-    except Exception as exception:
-        print(exception)
+    except Exception:
+        pass
