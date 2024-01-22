b08da0ec428395c44d8d8118d2062a0089404bf8
Octavian Irimia <octavian.irimia0309@gmail.com>
2024-01-22 20:31:14 +0200

    Updated main

--- Code/main.py
+++ Code/main.py
@@ -11,6 +11,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-# Test url: https://www.youtube.com/watch?v=_TtFTSv42bY
-# Age restricted test url: https://www.youtube.com/watch?v=fYH8eSiOf5I
